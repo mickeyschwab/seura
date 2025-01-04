b7d9dce51c2e0126b0547606ff82ce341293d5ef
Mickey Schwab <mickeyschwab@gmail.com>
2025-01-03 23:05:28 -0600

    Fix __init__ to include config values

--- examples/demo.py
+++ examples/demo.py
@@ -1,9 +1,9 @@
 from seura import SeuraClient
 
 client = SeuraClient(ip_address="10.0.0.114")
-client.power_on()
-# print(client.query_power())
-# print(client.query_volume())
+# client.power_on()
+print(client.query_power())
+print(client.query_volume())
 # print(client.query_channel())
 # print(client.query_input())
 # print(client.set_volume(10))
